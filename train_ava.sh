c02d57f3c62328b02982b6477f3b73219a526dd7
JasonHuang0119 <143667868+JasonHuang0119@users.noreply.github.com>
2024-12-19 11:50:15 +0800

    Update train_ava.sh

--- train_ava.sh
+++ train_ava.sh
@@ -2,12 +2,12 @@
 python train.py \
         --cuda \
         -d ava_v2.2 \
-        -v yowo_v2_slowfast \
+        -v yowo_v2_large \
         --root /home/jason/YOWOv2_cow/dataset/ \
         --num_workers 4 \
         --eval_epoch 1 \
         --eval \
-        --max_epoch 150 \
+        --max_epoch 20 \
         --lr_epoch 3 4 5 6 \
         -lr 0.0001 \
         -ldr 0.5 \
@@ -15,4 +15,3 @@ python train.py \
         -accu 16 \
         -K 16 \
         -tbs 16 \
-         -r /home/jason/YOWOv2_cow/weights/ava_v2.2/yowo_v2_slowfast/2024_6_2_Eiou_for_loss_yowov2/yowo_v2_slowfast_epoch_100.pth \
